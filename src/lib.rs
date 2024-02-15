178ecb8eaeffcafb13c7b927cb5ed96b028b5396
mtshr <mtshr319@gmail.com>
2024-02-15 13:46:40 +0900

    Implement pop in/out effect

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ mod vec3;
 use std::{array, f64::consts::TAU};
 
 use vec3::Vec3;
-use web_sys::{window, CanvasRenderingContext2d, HtmlCanvasElement};
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
 use yew::prelude::*;
 
 use wasm_bindgen::prelude::*;
@@ -12,9 +12,21 @@ enum Msg {
 	Tick,
 }
 
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum State {
+	PopIn { timer: u32 },
+	Stable { timer: u32 },
+	PopOut { timer: u32 },
+}
+
+impl State {
+	const STABLE_DURATION: u32 = 5 * 60;
+}
+
 struct AnimationCanvas {
 	canvas: NodeRef,
 	time: u32,
+	state: State,
 	angle: [f64; 3],
 	angle_velocity: [f64; 3],
 	vertices: Vec<Vec3>,
@@ -37,6 +49,14 @@ fn cube() -> Vec<Vec3> {
 	v
 }
 
+fn logistic(x: f64, a: f64, k: f64) -> f64 {
+	1. / (1. + a * (-k * x).exp())
+}
+
+fn in_out_coefficient(x: f64) -> f64 {
+	logistic(x, 500., 0.5)
+}
+
 impl Component for AnimationCanvas {
 	type Message = Msg;
 	type Properties = ();
@@ -47,6 +67,7 @@ impl Component for AnimationCanvas {
 		Self {
 			canvas: NodeRef::default(),
 			time: 0,
+			state: State::PopIn { timer: 0 },
 			angle: array::from_fn(|_| rand::random::<f64>() * TAU),
 			angle_velocity: [0.01, 0.02, 0.03],
 			vertices: cube(),
@@ -65,7 +86,7 @@ impl Component for AnimationCanvas {
 			Msg::Tick => {
 				self.tick();
 				self.render();
-				window()
+				web_sys::window()
 					.unwrap()
 					.request_animation_frame(self.callback.as_ref().unchecked_ref())
 					.unwrap();
@@ -84,6 +105,27 @@ impl Component for AnimationCanvas {
 impl AnimationCanvas {
 	fn tick(&mut self) {
 		self.time = self.time.wrapping_add(1);
+
+		match &mut self.state {
+			State::PopIn { timer } => {
+				*timer += 1;
+				if in_out_coefficient(*timer as f64 - 3. * self.vertices.len() as f64) > 1. - 1e-8 {
+					self.state = State::Stable { timer: 0 };
+				}
+			}
+			State::Stable { timer } => {
+				*timer += 1;
+				if *timer == State::STABLE_DURATION {
+					self.state = State::PopOut { timer: 0 };
+				}
+			}
+			State::PopOut { timer } => {
+				*timer += 1;
+				if in_out_coefficient(*timer as f64 - 3. * self.vertices.len() as f64) > 1. - 1e-8 {
+					self.state = State::PopIn { timer: 0 };
+				}
+			}
+		}
 
 		// update angle
 		for (a, v) in self.angle.iter_mut().zip(self.angle_velocity.iter_mut()) {
@@ -135,16 +177,23 @@ impl AnimationCanvas {
 		for (i, Vec3 { x, y, z }) in vertices.into_iter() {
 			let t = (self.time & 255) as f64 / 256. * TAU + (123454321 % (i * i + 1)) as f64;
 			let (dx, dy) = {
-				let a = (i * i % 5) as f64;
+				let a = (i % 5) as f64;
 				let b = (i % 3) as f64;
 				((a * t).cos(), (b * t).cos())
 			};
 
 			let gray_scale = (192. + (x / 2. - y + z) * 32.).clamp(0f64, 255.) as u8;
 			let co = z * 1.5 + 60.;
-			let x = center.x + (x + 0.03 * dx) * co;
-			let y = center.y + (y + 0.03 * dy) * co;
-			let r = 8.3 + 1.25 * t.cos() + z / 2.5;
+			let x = center.x + (x + 0.05 * dx) * co;
+			let y = center.y + (y + 0.05 * dy) * co;
+			let base_r = 8.3 + 1.25 * t.cos() + z / 2.5;
+			let r = match self.state {
+				State::PopIn { timer } => base_r * in_out_coefficient(timer as f64 - 3. * i as f64),
+				State::PopOut { timer } => {
+					base_r * (1. - in_out_coefficient(timer as f64 - 3. * i as f64))
+				}
+				_ => base_r,
+			};
 			ctx.set_fill_style(&format!("#{x:02X}{x:02X}{x:02X}", x = gray_scale).into());
 			ctx.begin_path();
 			ctx.arc(x, y, r, 0f64, TAU).unwrap();
